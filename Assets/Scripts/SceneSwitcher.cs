da1c1d9eb48e8e7043dbf686dfecbcd896fd9cbf
DaisyBit <DaisyBit@users.noreply.github.com>
2026-04-15 13:04:44 -0400

    Added main menu and pause menu

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        if (pauseMenuUI != null)
+            pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void LoadSampleScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
